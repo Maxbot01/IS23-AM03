c13ac214071169f8b962e91ef1df6c15f70ea103
Maxbot01 <126093619+Maxbot01@users.noreply.github.com>
2023-03-17 17:29:02 +0100

    bozzery LivingRoom

---
+++ src/main/java/it/polimi/ingsw/model/modelSupport/BoardCard.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.model.modelSupport;
+
+public class BoardCard {
+}
--- src/main/java/it/polimi/ingsw/model/modelSupport/Livingroom.java
+++ src/main/java/it/polimi/ingsw/model/modelSupport/Livingroom.java
@@ -1,12 +1,77 @@
 package it.polimi.ingsw.model.modelSupport;
+import java.util.ArrayList;
+import java.util.List;
 
-import it.polimi.ingsw.model.Game;
+
+public class LivingRoom{
+
+    private BoardCard [][] pieces;
+    private List<BoardCard> allCards = new ArrayList<BoardCard>;
+    private int indexOfDeckCard;
+    final int DIM = 9;
+
+    LivingRoom lr = new LivingRoom(int NumOfPLayers){
+
+        shuffle();
+        BoardCard[][] pieces = new BoardCard[DIM][DIM];
+        if (NumOfPLayers == 2){
+            for (int i = 1; i < righe; i++) {
+            for (int j = 0; j < colonne; j++) {
+                matrice[i][j] = rand.nextInt(100); // generare numeri casuali tra 0 e 99
+            }
+        }
 
 
-public class Livingroom extends Game {
-    //added by enrico
-    //added again
+
+
+
+    }
+    BoardCard[] shuffle(){
+                // Definisci la dimensione dell'array
+                int size = 10;
+
+                // Crea un array di interi
+                int[] array = new int[size];
+
+                // Crea un oggetto Random per generare numeri casuali
+                Random random = new Random();
+
+                // Popola l'array con numeri casuali
+                for (int i = 0; i < size; i++) {
+                    array[i] = random.nextInt(100); // Genera un numero casuale tra 0 e 99
+                }
+
+                // Stampa l'array
+                for (int i = 0; i < size; i++) {
+                    if (array[i] < 22)
+
+                }
+            }
+        }
+        }
+    }
+    BoardCard[][] getPieces() {
+        return pieces;
+    }
+
+    void refillBoard(int NumOfPlayers){
+
+    }
+
+    void updateBoard(ArrayList<Pair<int,int>>){
+
+    }
+    Boolean[][] calculateSelectable(){
+
+    }
+
+
+
+
+
+
+
+
 }
-
 
 
