e9b1f24ce76b77eb384e63cc1b9be32fd80901d2
Maxbot01 <126093619+Maxbot01@users.noreply.github.com>
2023-03-17 18:32:15 +0100

    bozzery LivingRoom

---
+++ src/main/java/it/polimi/ingsw/model/modelSupport/LivingRoom.java
@@ -0,0 +1,54 @@
+package it.polimi.ingsw.model.modelSupport;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class LivingRoom{
+
+    private BoardCard [][] pieces;
+    private List<BoardCard> allCards = new ArrayList<BoardCard>;
+    private int indexOfDeckCard;
+    final int DIM = 9;
+
+    LivingRoom lr = new LivingRoom(int NumOfPLayers){
+
+
+
+
+
+
+
+    }
+    BoardCard[] shuffle(){
+
+
+    }
+}
+        }
+                }
+                }
+                BoardCard[][] getPieces() {
+                return pieces;
+                }
+
+                void refillBoard(int NumOfPlayers){
+
+                }
+
+                void updateBoard(ArrayList<Pair<int,int>>){
+
+        }
+        Boolean[][] calculateSelectable(){
+
+        }
+
+
+
+
+
+
+
+
+        }
+
+
