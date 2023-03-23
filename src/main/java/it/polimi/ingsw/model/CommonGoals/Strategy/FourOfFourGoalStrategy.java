27ecd6c250f11b8fa99944f6eb02c9afaf551dcc
happy <riccardo1.antonelli@mail.polimi.it>
2023-03-23 19:45:21 +0100

    fixed stuff and some forbidden tests

--- src/main/java/it/polimi/ingsw/model/CommonGoals/Strategy/FourOfFourGoalStrategy.java
+++ src/main/java/it/polimi/ingsw/model/CommonGoals/Strategy/FourOfFourGoalStrategy.java
@@ -28,7 +28,8 @@ public class FourOfFourGoalStrategy implements CommonGoalStrategy{
         for(int i = 0; i < rows && complete == 0; i++){
             for(int j = 0; j < cols && complete == 0; j++){
                 if(Copy[i][j] != null) {
-                    combinations += findCombination(i, j, Copy, coordinates, threeAdiac);
+                    findCombination(i, j, Copy, coordinates, threeAdiac);
+                    combinations = coordinates.size();
                     if(combinations / 4 == 0){
                         /* elimina le tessere nella copy con coordinates */
                         for (Pair<Integer,Integer> position: coordinates) {
@@ -47,9 +48,9 @@ public class FourOfFourGoalStrategy implements CommonGoalStrategy{
                         coordinates.removeAll(coordinates);
                         threeAdiac.removeAll(threeAdiac);
                     } else if (combinations / 4 == 2) {
-                        correctLines += twoLinesAlgorithm();
+                        correctLines += eightAndTwelveAlgorithm(threeAdiac, Mat);
                     } else if (combinations / 4 == 3) {
-                        correctLines +=;
+                        correctLines += eightAndTwelveAlgorithm(threeAdiac, Mat);
                     } else if (combinations / 4 == 4) {
                         correctLines +=;
                     }
@@ -59,114 +60,100 @@ public class FourOfFourGoalStrategy implements CommonGoalStrategy{
                 }
             }
         }
-
-
         if(complete == 1){
             return true;
         }else{
             return false;
         }
     }
-    private int findCombination(int x, int y, BoardCard[][] mat, ArrayList<Pair<Integer,Integer>> coordinates,
+    private void findCombination(int x, int y, BoardCard[][] mat, ArrayList<Pair<Integer,Integer>> coordinates,
                                 ArrayList<Pair<Integer,Integer>> threeAdiac){
         int rows = mat.length;
         int cols = mat[0].length;
         colorType colore = mat[x][y].getColor();
-        int cardUguali = 1;
         int cont = 0;
 
         Pair<Integer,Integer> startingCard = new Pair<>(x,y);
         coordinates.add(startingCard);
 
-        for(int i = x; i < rows; i++){ /* NON MI SERVONO I FOR, è ricorsivo */
-            for(int j = y; j < cols; j++){
-                Pair<Integer,Integer> cardW = new Pair<>(i,j-1); /* NEANCHE QUSTI MI SERVONO */
-                Pair<Integer,Integer> cardN = new Pair<>(i-1,j);
-
-                if(j-1 >= 0){ /* card -> W */
-                    if(colore.equals(mat[i][j-1].getColor())){
-                        cardUguali += findCombination(i, j-1, mat, coordinates, threeAdiac);
-                        cont++;
-                    }
-                }
-                if(i+1 < rows){ /* card -> S */
-                    if(colore.equals(mat[i+1][j].getColor())){
-                        cardUguali += findCombination(i+1, j, mat, coordinates, threeAdiac);
-                        cont++;
-                    }
-                }
-                if(j+1 < cols){ /* card -> E */
-                    if(colore.equals(mat[i][j+1].getColor())){
-                        cardUguali += findCombination(i, j+1, mat, coordinates, threeAdiac);
-                        cont++;
-                    }
-                }
-                if(i-1 >= 0){ /* card -> N */
-                    if(colore.equals(mat[i-1][j].getColor())){
-                        cardUguali += findCombination(i-1, j, mat, coordinates, threeAdiac);
-                        cont++;
-                    }
-                }
-                if(cont >= 3){
-                    threeAdiac.add(startingCard);
-                }
+        if(y-1 >= 0){ /* card -> W */
+            if(colore.equals(mat[x][y-1].getColor())){
+                findCombination(x, y-1, mat, coordinates, threeAdiac);
+                cont++;
             }
+        }
+        if(x+1 < rows){ /* card -> S */
+            if(colore.equals(mat[x+1][y].getColor())){
+                findCombination(x+1, y, mat, coordinates, threeAdiac);
+                cont++;
+            }
+        }
+        if(y+1 < cols){ /* card -> E */
+            if(colore.equals(mat[x][y+1].getColor())){
+                findCombination(x, y+1, mat, coordinates, threeAdiac);
+                cont++;
+            }
+        }
+        if(x-1 >= 0){ /* card -> N */
+            if(colore.equals(mat[x-1][y].getColor())){
+                findCombination(x-1, y, mat, coordinates, threeAdiac);
+                cont++;
+            }
+        }
+        if(cont >= 3){
+            threeAdiac.add(startingCard);
         }
     }
     /* coord corrisponde a threeAdiac */
-    private int twoLinesAlgorithm(ArrayList<Pair<Integer,Integer>> coord, BoardCard[][] mat){
+    private int eightAndTwelveAlgorithm(ArrayList<Pair<Integer,Integer>> coord, BoardCard[][] mat){
         ArrayList<Pair<Integer,Integer>> seen = new ArrayList<>();
         int success = 0;
 
-
-
-        for(int z = 0; z < coord.size() && success == 0; z++){
 /* Nel caso di n/4 = 8 basta fare un singolo controllo sulle threeAdiac, non ciclare tutte quelle trovate */
-            Pair<Integer,Integer> cross = new Pair<>(coord.get(z).getFirst(), coord.get(z).getSecond());
+        Pair<Integer,Integer> cross = new Pair<>(coord.get(0).getFirst(), coord.get(0).getSecond());
 
-            if(mat[cross.getFirst()][cross.getSecond()].getColor().equals(
-                    mat[cross.getFirst()][cross.getSecond()-1].getColor())){
-                seen.add(cross);
-                calculateBranchSize(mat, cross.getFirst(), cross.getSecond()-1, seen);
-                if(seen.size() >= 4){
-                    success = 1;
-                }
+        if(mat[cross.getFirst()][cross.getSecond()].getColor().equals(
+                mat[cross.getFirst()][cross.getSecond()-1].getColor())){
+            seen.add(cross);
+            calculateBranchSize(mat, cross.getFirst(), cross.getSecond()-1, seen);
+            if(seen.size() >= 4){
+                success = 1;
             }
-            if(success == 0 && mat[cross.getFirst()][cross.getSecond()].getColor().equals(
-                    mat[cross.getFirst()+1][cross.getSecond()].getColor())){
-                seen.add(cross);
-                calculateBranchSize(mat, cross.getFirst()+1, cross.getSecond(), seen);
-                if(seen.size() >= 4){
-                    success = 1;
-                }
+        }
+        if(success == 0 && mat[cross.getFirst()][cross.getSecond()].getColor().equals(
+                mat[cross.getFirst()+1][cross.getSecond()].getColor())){
+            seen.add(cross);
+            calculateBranchSize(mat, cross.getFirst()+1, cross.getSecond(), seen);
+            if(seen.size() >= 4){
+                success = 1;
             }
-            if(success == 0 && mat[cross.getFirst()][cross.getSecond()].getColor().equals(
-                    mat[cross.getFirst()][cross.getSecond()+1].getColor())){
-                seen.add(cross);
-                calculateBranchSize(mat, cross.getFirst(), cross.getSecond()+1, seen);
-                if(seen.size() >= 4){
-                    success = 1;
-                }
+        }
+        if(success == 0 && mat[cross.getFirst()][cross.getSecond()].getColor().equals(
+                mat[cross.getFirst()][cross.getSecond()+1].getColor())){
+            seen.add(cross);
+            calculateBranchSize(mat, cross.getFirst(), cross.getSecond()+1, seen);
+            if(seen.size() >= 4){
+                success = 1;
             }
-            if(success == 0 && mat[cross.getFirst()][cross.getSecond()].getColor().equals(
-                    mat[cross.getFirst()-1][cross.getSecond()].getColor())){
-                seen.add(cross);
-                calculateBranchSize(mat, cross.getFirst()-1, cross.getSecond(), seen);
-                if(seen.size() >= 4){
-                    success = 1;
-                }
+        }
+        if(success == 0 && mat[cross.getFirst()][cross.getSecond()].getColor().equals(
+                mat[cross.getFirst()-1][cross.getSecond()].getColor())){
+            seen.add(cross);
+            calculateBranchSize(mat, cross.getFirst()-1, cross.getSecond(), seen);
+            if(seen.size() >= 4){
+                success = 1;
             }
         }
 /* CASO PER N/4=12 ----------------------------------------------- */
 
         for(int z = 0; z < coord.size() && success == 0; z++){
             /* Nel caso di n/4 = 8 basta fare un singolo controllo sulle threeAdiac, non ciclare tutte quelle trovate */
-            Pair<Integer,Integer> cross = new Pair<>(coord.get(z).getFirst(), coord.get(z).getSecond());
+            /* Pair<Integer,Integer> */ cross = new Pair<>(coord.get(z).getFirst(), coord.get(z).getSecond());
             int branchCont = 0;
 
             if(mat[cross.getFirst()][cross.getSecond()].getColor().equals(
                     mat[cross.getFirst()][cross.getSecond()-1].getColor())){
-                seen.add(cross);
+                /* seen.add(cross); NON DEVO AGGIUNGERE cross per il 12 */
                 calculateBranchSize(mat, cross.getFirst(), cross.getSecond()-1, seen);
                 if(seen.size() >= 4){
                     branchCont++;
@@ -174,7 +161,7 @@ public class FourOfFourGoalStrategy implements CommonGoalStrategy{
             }
             if(branchCont < 2 && mat[cross.getFirst()][cross.getSecond()].getColor().equals(
                     mat[cross.getFirst()+1][cross.getSecond()].getColor())){
-                seen.add(cross);
+                /* seen.add(cross); */
                 calculateBranchSize(mat, cross.getFirst()+1, cross.getSecond(), seen);
                 if(seen.size() >= 4){
                     branchCont++;
@@ -182,7 +169,7 @@ public class FourOfFourGoalStrategy implements CommonGoalStrategy{
             }
             if(branchCont < 2 && mat[cross.getFirst()][cross.getSecond()].getColor().equals(
                     mat[cross.getFirst()][cross.getSecond()+1].getColor())){
-                seen.add(cross);
+                /* seen.add(cross); */
                 calculateBranchSize(mat, cross.getFirst(), cross.getSecond()+1, seen);
                 if(seen.size() >= 4){
                     branchCont++;
@@ -190,7 +177,7 @@ public class FourOfFourGoalStrategy implements CommonGoalStrategy{
             }
             if(branchCont < 2 && mat[cross.getFirst()][cross.getSecond()].getColor().equals(
                     mat[cross.getFirst()-1][cross.getSecond()].getColor())){
-                seen.add(cross);
+                /* seen.add(cross); */
                 calculateBranchSize(mat, cross.getFirst()-1, cross.getSecond(), seen);
                 if(seen.size() >= 4){
                     branchCont++;
