c933e39e4dd0dd21bf937789552b0178767abff9
Enrico Alberti <enricoalbe@icloud.com>
2023-04-02 15:26:38 +0200

    some networking

---
+++ src/main/java/it/polimi/ingsw/client/ClientMain.java
@@ -0,0 +1,39 @@
+package it.polimi.ingsw.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class ClientMain implements Runnable {
+    private Socket socket;
+    public ClientMain(Socket socket) {
+        this.socket = socket;
+    }
+    public void run() {
+        try {
+            Scanner in = new Scanner(socket.getInputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+            // Leggo e scrivo nella connessione finche' non ricevo "quit"
+            while (true) {
+                String line = in.nextLine();
+                if (line.equals("quit")) {
+                    break;
+                } else {
+                    out.println("Received: " + line);
+                    out.flush();
+                }
+            }
+            // Chiudo gli stream e il socket
+            in.close();
+            out.close();
+            socket.close();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
+
