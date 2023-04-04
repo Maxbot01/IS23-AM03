d3a8200eebf11b500f0207a456c43117bd56912d
AlessandroAssini <alessandro.assini@mail.polimi.it>
2023-04-04 17:29:47 +0200

    CommonGoalsTest.java

--- src/test/java/it/polimi/ingsw/model/CommonGoals/CommonGoalsTest.java
+++ src/test/java/it/polimi/ingsw/model/CommonGoals/CommonGoalsTest.java
@@ -3,10 +3,16 @@ package it.polimi.ingsw.model.CommonGoals;
 import it.polimi.ingsw.model.CommonGoals.Strategy.CommonGoalStrategy;
 import it.polimi.ingsw.model.CommonGoals.Strategy.SixOfTwoGoalStrategy;
 import it.polimi.ingsw.model.CommonGoals.Strategy.TriangularGoalStrategy;
+import it.polimi.ingsw.model.modelSupport.Player;
 import it.polimi.ingsw.model.modelSupport.Shelf;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.System.out;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommonGoalsTest {
@@ -28,6 +34,8 @@ class CommonGoalsTest {
         firstGoal = new TriangularGoalStrategy();
         prova.setFirstGoal(firstGoal);
         assertSame(prova.getFirstGoal(), firstGoal);
+        // output prova
+        out.println("First goal: " + prova.getFirstGoal());
     }
 
     /**
@@ -38,10 +46,36 @@ class CommonGoalsTest {
         secondGoal = new SixOfTwoGoalStrategy();
         prova.setSecondGoal(secondGoal);
         assertSame(prova.getSecondGoal(), secondGoal);
+        // output prova
+        out.println("Second goal: " + prova.getSecondGoal());
     }
 
     @Test
-    void testCalculateAllPoints() {
+    public void testCalculateAllPoints() {
+        Player player1 = new Player("player1");
+        Player player2 = new Player("player2");
+        List<Player> players = Arrays.asList(player1, player2);
+        int numOfPlayers = players.size();
 
+        int expectedPointsPlayer1 = 0;
+        int expectedPointsPlayer2 = 0;
+
+        /* Player 1 has not completed the first goal */
+        boolean goalCompleted = true;
+        if (goalCompleted) {
+            prova.calculateAllPoints(player1, numOfPlayers);
+            expectedPointsPlayer1 = numOfPlayers > 2 ? 6 : 4;
+        }
+
+        /* Player 2 has completed the first goal */
+        goalCompleted = prova.getFirstGoal().goalCompleted(player2.getPlayersShelf().getShelfCards());
+        if (goalCompleted) {
+            prova.calculateAllPoints(player2, numOfPlayers);
+            expectedPointsPlayer2 = numOfPlayers > 2 ? 8 : 4;
+        }
+
+        /* Check that the points were correctly assigned */
+        Assertions.assertEquals(4, expectedPointsPlayer1);
+        Assertions.assertEquals(0, expectedPointsPlayer2);
     }
 }
