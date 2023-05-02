0df9425999d93105799a601def49f4c739616268
happy <riccardo1.antonelli@mail.polimi.it>
2023-05-02 15:38:53 +0200

    fixed stuff and some forbidden tests

---
+++ src/main/java/it/polimi/ingsw/model/messageModel/errorMessages/ErrorType.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.model.messageModel.errorMessages;
+
+public enum ErrorType {
+    selectedColumnsError,
+    acceptFinishedGameError,
+    selectedCardsMessageError,
+    wrongPassword,
+    usernameAlreadyExists
+}
